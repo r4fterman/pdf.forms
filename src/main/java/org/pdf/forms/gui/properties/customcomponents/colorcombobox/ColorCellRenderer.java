02bfe19aca04279355335fae9b30f22996a34fae
Roman Wolf <r4fterman@gmail.com>
2020-10-15 11:18:15 +0200

    To avoid field instantiation extend DefaultListCellRenderer directly.

--- src/main/java/org/pdf/forms/gui/properties/customcomponents/colorcombobox/ColorCellRenderer.java
+++ src/main/java/org/pdf/forms/gui/properties/customcomponents/colorcombobox/ColorCellRenderer.java
@@ -1,25 +1,17 @@
 package org.pdf.forms.gui.properties.customcomponents.colorcombobox;
 
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-
-import javax.swing.DefaultListCellRenderer;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.ListCellRenderer;
+import javax.swing.*;
+import java.awt.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ColorCellRenderer implements ListCellRenderer<Object> {
+public class ColorCellRenderer extends DefaultListCellRenderer {
 
     private final Logger logger = LoggerFactory.getLogger(ColorCellRenderer.class);
 
     // width doesn't matter as combobox will size
     private final Dimension preferredSize = new Dimension(0, 20);
-
-    private DefaultListCellRenderer defaultRenderer = new DefaultListCellRenderer();
 
     @Override
     public Component getListCellRendererComponent(
@@ -28,13 +20,13 @@ public class ColorCellRenderer implements ListCellRenderer<Object> {
             final int index,
             final boolean isSelected,
             final boolean cellHasFocus) {
-        final JLabel renderer = (JLabel) defaultRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+        final JLabel renderer = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
 
         if (value instanceof Color) {
             renderer.setBackground((Color) value);
             renderer.setText("");
-            //} else if (value instanceof String && value.equals("Custom")) {
-            // Custom color found
+        } else if (value instanceof String && value.equals("Custom")) {
+            logger.info("Custom color found");
         } else {
             logger.warn("Unexpected list cell renderer value {} of class {}", value, value.getClass().getCanonicalName());
         }
